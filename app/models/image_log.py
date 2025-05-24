03fc1058e486396a6a73aad026f0f1c749bb6164
abdelrahman-a99 <a.ahmed2299>
2025-05-24 06:33:30 +0300

    edited image_log.py file to add the image data and edited in the upload.py to read the image from the database and made the download endpoint

--- app/models/image_log.py
+++ app/models/image_log.py
@@ -4,3 +4,4 @@ class ImageLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(120), nullable=False)
     processed = db.Column(db.Boolean, default=False)
+    image_data = db.Column(db.Text, nullable=True)
